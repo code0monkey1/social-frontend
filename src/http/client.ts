d270c46a286e7d7534982345ff960d356793b6e1
code0monkey1 <chiranjeevwork@gmail.com>
2024-09-24 12:44:06 +0530

    add auto refresh token

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
@@ -8,3 +9,59 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+// you can use an axios interceptor to  verify that if the accessToken is invalid / expired ( you can use axios.interceptors.response.use() ) , and stop the request to reach the client , and proceed to retry and refresh the token, to get a new accessToken and refreshToken using the refresh endpoint
+export const refreshRequest = async () => {
+  // we will use plane axios for this
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    // we will see if the response is a 401 ( i.e accessToken was expired )
+
+    const originalRequest = error.config; // this is the original request that was made
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      // if the response status is 401 and the original request was not retried
+      originalRequest._retry = true; // we set the _retry property to true
+
+      try {
+        const originalHeaders = { ...originalRequest.headers }; // we get the original headers
+
+        // we need to make a refreshRequest , which does not involve the defined api instance call ( as it will lose the previous request information  [ self request ] )
+
+        await refreshRequest(); // we make a request to the refresh endpoint to get a new accessToken and refreshToken
+
+        // now you can send  a new request using the valid tokens ( that have already been sent as cookies in the request )
+
+        // we return the new request with the original headers
+        return api.request({
+          ...originalRequest,
+          headers: originalHeaders,
+        });
+      } catch (error) {
+        console.log("refresh token was not found");
+
+        // logout the user from the frontend ( i.e delete tokens) if even the refresh token was not found on server side
+
+        // getState() get's you the whole store object , from which you can call logout to clear the user information from the store
+        useAuthStore.getState().logout();
+
+        // this will result in an error state on the client , showing that the refreshToken was not found , and the user will have to login again !
+
+        return Promise.reject(error);
+      }
+    }
+
+    // if the response status is not 401 (i.e any other server error) , we will return the error as normal
+    return Promise.reject(error);
+  }
+);
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { Outlet } from "react-router";
 import { self } from "../http/api";
 import { useAuthStore } from "../store";
+import { AxiosError } from "axios";
 
 const getSelf = async () => {
   const { data } = await self();
@@ -17,6 +18,14 @@ const Root = () => {
   const { data, isLoading } = useQuery({
     queryKey: ["self"],
     queryFn: getSelf,
+    retry(failureCount, error) {
+      // avoid retrying if the failure code is 401 ( in which case we'll use refresh token to get a new access token )
+
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        return false;
+      }
+      return failureCount < 3;
+    },
   });
 
   useEffect(() => {
